bc305746c4fdd9a4caa1ba0f1e1d8ac5995b218f
Giselle <gisellec60@gmail.com>
2023-05-22 11:14:52 -0400

    getting undefined

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,16 @@
 import React from "react";
 
 function NavBar() {
-  const links = ["home", "about", "projects"];
+  const links = [{id:1, name:"home"}, {id:2, name:"about"}, {id:3, name:"projects"}];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const projCards = links.map((linkObj) => {
+    return <a href="#home" key={linkObj.id}>{linkObj.name}</a>
+  })
+  return (
+   <nav>
+      {projCards}
+  </nav>
+  )
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,17 @@
 import React from "react";
-import ProjectItem from "./ProjectItem";
+//import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  console.log("this is projects", projects);
+  const projList = projects.map((listObj) => {
+    return <ProjectList key={listObj.id} name={listObj.name} about={listObj.about} tech={listObj.technologies} />
+  })
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projList}
+        </div>
     </div>
   );
 }
